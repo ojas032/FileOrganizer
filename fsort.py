d76be9a212b60911c4bbb416bebab9cdffc800d8
Ojas Gupta <44767396+ojas032@users.noreply.github.com>
2019-07-06 00:25:55 +0530

    Add files via upload

--- fsort.py
+++ fsort.py
@@ -3,52 +3,60 @@ import glob
 import shutil 
 
 
-os.chdir("C:\\Users\Ojas Gupta\Pictures")
-print(os.listdir())
+def organise(add):
+    os.chdir(add)
+    print(os.listdir())
 
-src="C:\\Users\Ojas Gupta\Pictures"
-l=["*.py","*.c","*.class","*.cpp","*.cs","*.h",".java","*.sh","*.swift","*.vb","*.jpeg","*.jpg","*.png","*.gif","*.tiff","*.txt","*.doc","*.docx","*.odt","*.pdf","*.rtf","*.tex","*.wps","*.wpd","*.3g2","*.3gp","*.avi","*.flv","*.h264","*.m4v","*.mkv","*.mov","*.mp4","*.mpg","*.mpeg","*.rm","*.swf","*.vob","*.wmv"]
+    src=add
+    l=["*.aif","*.cda","*.mid","*.midi","*.mp3","*.mpa","*.ogg","*.wav","*.wma","*.wpl","*.py","*.c","*.class","*.cpp","*.cs","*.h",".java","*.sh","*.swift","*.vb","*.jpeg","*.jpg","*.png","*.gif","*.tiff","*.txt","*.doc","*.docx","*.odt","*.pdf","*.rtf","*.tex","*.wps","*.wpd","*.3g2","*.3gp","*.avi","*.flv","*.h264","*.m4v","*.mkv","*.mov","*.mp4","*.mpg","*.mpeg","*.rm","*.swf","*.vob","*.wmv"]
+
+    for j in l:            
+        for files in glob.glob(j):
+            #pictures
+            if(j in ["*.jpeg","*.jpg","*.png","*.gif","*.tiff"]):
+                if not os.path.exists("fspictures"):
+                    os.makedirs("fspictures")
+                p="/fspictures"
+                dest=src+p
+                shutil.move(files,dest)
+            #documents      
+            elif(j in ["*.txt","*.doc","*.docx","*.odt","*.pdf","*.rtf","*.tex","*.wps","*.wpd"]):
+                if not os.path.exists("fsDocuments"):
+                    os.makedirs("fsDocuments")
+                p="/fsDocuments"
+                dest=src+p
+                shutil.move(files,dest)
+            #videos    
+            elif(j in ["*.3g2","*.3gp","*.avi","*.flv","*.h264","*.m4v","*.mkv","*.mov","*.mp4","*.mpg","*.mpeg","*.rm","*.swf","*.vob","*.wmv"]):    
+                if not os.path.exists("fsMovies"):
+                    os.makedirs("fsMovies")
+                p="/fsMovies"
+                dest=src+p
+                shutil.move(files,dest)
+
+            #programfiles:
+            elif(j in ["*.py","*.c","*.class","*.cpp","*.cs","*.h",".java","*.sh","*.swift","*.vb"]):
+                if not os.path.exists("fsComp-Programs"):
+                    os.makedirs("fsComp-Programs")
+                p="/fsComp-Programs"
+                dest=src+p
+                shutil.move(files,dest)
+            #Audio
+            elif(j in ["*.aif","*.cda","*.mid","*.midi","*.mp3","*.mpa","*.ogg","*.wav","*.wma","*.wpl"]):
+                if not os.path.exists("fsAudio"):
+                    os.makedirs("fsAudio")
+                p="/fsAudio"
+                dest=src+p
+                shutil.move(files,dest)
+            #Compressed    
+            elif(j in ["*.7z","*.arj","*.deb","*.pkg","*.rar","*.rpm","*.tar.gz","*.z","*.zip"]):
+                if not os.path.exists("fsCompressed"):
+                    os.makedirs("fsCompressed")
+                p="fsCompressed"
+                dest=src+p
+                shutil.move(files,dest)       
 
 
-
-for j in l:
-    if(j in ["*.jpeg","*.jpg","*.png","*.gif","*.tiff"]):
-        if not os.path.exists("fspictures"):
-            os.makedirs("fspictures")
-        p="/fspictures"
-        dest=src+p
-    elif(j in ["*.txt","*.doc","*.docx","*.odt","*.pdf","*.rtf","*.tex","*.wps","*.wpd"]):
-        if not os.path.exists("fsDocuments"):
-            os.makedirs("fsDocuments")
-        p="/fsDocuments"
-        dest=src+p
-    elif(j in ["*.3g2","*.3gp","*.avi","*.flv","*.h264","*.m4v","*.mkv","*.mov","*.mp4","*.mpg","*.mpeg","*.rm","*.swf","*.vob","*.wmv"]):
-        if not os.path.exists("fsMovies"):
-    
-            os.makedirs("fsMovies")
-        p="/fsMovies"
-        dest=src+p
-    elif(j in ["*.py","*.c","*.class","*.cpp","*.cs","*.h",".java","*.sh","*.swift","*.vb"]):
-        if not os.path.exists("fsComp-Programs"):
-            os.makedirs("fsComp-Programs")
-        p="/fsComp-Programs"
-        dest=src+p
-
-        
-    for files in glob.glob(j):
-        #pictures
-        if(j in ["*.jpeg","*.jpg","*.png","*.gif","*.tiff"]):
-            shutil.move(files,dest)
-        #documents      
-        elif(j in ["*.txt","*.doc","*.docx","*.odt","*.pdf","*.rtf","*.tex","*.wps","*.wpd"]):
-            shutil.move(files,dest)
-        #videos    
-        elif(j in ["*.3g2","*.3gp","*.avi","*.flv","*.h264","*.m4v","*.mkv","*.mov","*.mp4","*.mpg","*.mpeg","*.rm","*.swf","*.vob","*.wmv"]):    
-            shutil.move(files,dest)
-
-         #programfiles:
-         elif(j in ["*.py","*.c","*.class","*.cpp","*.cs","*.h",".java","*.sh","*.swift","*.vb"]):
-             shutil.move(files,dest)   
 
 
 
--- script.py
+++ script.py
@@ -1,7 +1,17 @@
+from tkinter import filedialog
 import tkinter as tk
 import fsort
 
 window=tk.Tk()
+content = tk.StringVar()
+def file(): 
+    window.filename =  filedialog.askdirectory()
+    print (window.filename)
+    set_Text=window.filename
+    content.set(set_Text)
+    fsort.organise(set_Text)
+    return
+
 window.geometry("400x180") 
 window.configure(background='black')
 label=tk.Label(window,text="FileSort",fg="mediumseagreen",bg="black",padx="100")
@@ -12,11 +22,12 @@ label=tk.Label(window,text="Enter the path of the folder",bg="black",fg="white",
 label.config(font=("Arial", 12))
 label.grid(row=1,column=0)
 
-e1=tk.Entry(width=30)
+
+e1=tk.Entry(width=30 ,textvariable=content)
 e1.grid(row=2,column=0)
 
 #b1=tk.Button(window,text="Sort",bg="mediumseagreen",fg="white",)
-b1 = tk.Button(window)
+b1 = tk.Button(window, command=file)
 img = tk.PhotoImage(file="Capture.png") # make sure to add "/" not "\"
 b1.config(image=img)
 b1.grid(row=2,column=1)
